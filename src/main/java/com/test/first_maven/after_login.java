8575a4d843cf4ec296f0af7050db7f24d266656c
Zhaoyi Zhou <502648055@qq.com>
2020-03-18 10:44:16 +0800

    commit

--- src/main/java/com/test/first_maven/after_login.java
+++ src/main/java/com/test/first_maven/after_login.java
@@ -6,10 +6,30 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JTable;
+import javax.swing.JEditorPane;
+import javax.swing.JTree;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JPopupMenu;
+import java.awt.Component;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JMenuItem;
+import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JLabel;
+import com.jgoodies.forms.factories.DefaultComponentFactory;
+import javax.swing.JDesktopPane;
+import javax.swing.JTabbedPane;
+import javax.swing.JSplitPane;
 
 public class after_login extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -32,11 +52,46 @@ public class after_login extends JFrame {
 	 */
 	public after_login() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 50, 1050, 600);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		textField = new JTextField();
+		textField.setBounds(728, 23, 200, 21);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JButton button = new JButton("搜索");
+		button.setBounds(931, 22, 93, 23);
+		contentPane.add(button);
+		
+		final Object rowData[][] = {
+				{"1","2","3"},
+				{"4","5","6"},
+		};
+		final String columnNames[] = {"one","two","three"};
+		table = new JTable(rowData, columnNames);
+		
+		table.setBounds(110, 139, 449, 286);
+		contentPane.add(table);
 	}
-
+	private static void addPopup(Component component, final JPopupMenu popup) {
+		component.addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+				if (e.isPopupTrigger()) {
+					showMenu(e);
+				}
+			}
+			public void mouseReleased(MouseEvent e) {
+				if (e.isPopupTrigger()) {
+					showMenu(e);
+				}
+			}
+			private void showMenu(MouseEvent e) {
+				popup.show(e.getComponent(), e.getX(), e.getY());
+			}
+		});
+	}
 }
