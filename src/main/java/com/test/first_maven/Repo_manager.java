ef7ffa359f531c5f1a189fba40672ba0d231f6bc
Narip2 <502648055@qq.com>
2020-03-27 23:37:37 +0800

    modified:   .classpath 	modified:   .settings/org.eclipse.jdt.core.prefs 	modified:   src/main/java/com/test/first_maven/Repo_manager.java

--- src/main/java/com/test/first_maven/Repo_manager.java
+++ src/main/java/com/test/first_maven/Repo_manager.java
@@ -20,6 +20,7 @@ import javax.swing.JInternalFrame;
 import javax.swing.JButton;
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
+import javax.swing.JProgressBar;
 
 public class Repo_manager extends JFrame {
 	
@@ -64,6 +65,38 @@ public class Repo_manager extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		
+		JList list = new JList();
+		list.setBounds(115, 10, 286, 347);
+		contentPane.add(list);
+		
+		JList list_1 = new JList();
+		list_1.setBounds(10, 446, 286, 273);
+		contentPane.add(list_1);
+		
+		JTextPane textPane = new JTextPane();
+		textPane.setBounds(419, 10, 894, 347);
+		contentPane.add(textPane);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setBounds(427, 446, 886, 273);
+		contentPane.add(textArea);
+		
+		JButton button = new JButton("刷新");
+		button.setBounds(10, 10, 93, 23);
+		contentPane.add(button);
+		
+		JButton button_1 = new JButton("同步");
+		button_1.setBounds(10, 32, 93, 23);
+		contentPane.add(button_1);
+		
+		JButton btnCommit = new JButton("Commit");
+		btnCommit.setBounds(10, 54, 93, 23);
+		contentPane.add(btnCommit);
+		
+		JButton btnPush = new JButton("Push");
+		btnPush.setBounds(10, 77, 93, 23);
+		contentPane.add(btnPush);
 				
 	}
 }
