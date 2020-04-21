3cf2a8f603a541d91067ee5be9244aa1c8b833eb
Narip2 <502648055@qq.com>
2020-04-21 18:23:52 +0800

    modified:   src/main/java/Demo/demo6.java 	deleted:    "~$\346\263\250\351\207\212.docx"

--- src/main/java/Demo/demo6.java
+++ src/main/java/Demo/demo6.java
@@ -1,60 +1,28 @@
 package Demo;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.tree.DefaultMutableTreeNode;
-import javax.swing.tree.DefaultTreeModel;
-
-import com.test.first_maven.SSH;
-
-import javax.swing.JTree;
-
-public class demo6 extends JFrame {
-
-	private JPanel contentPane;
-	private DefaultTreeModel tree_model;
-	private DefaultMutableTreeNode Node;
-
-	/**
-	 * Launch the application.
-	 */
+public class demo6 {
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					demo6 frame = new demo6();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the frame.
-	 */
-	public demo6() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 758, 511);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		
-		
-		SSH ssh = new SSH();
-		ssh.SetProName("demo");
-		ssh.SetUserName("narip2");
-		Node = ssh.GetAllFiles("demo", "master");
-		tree_model = new DefaultTreeModel(Node);
-		JTree tree = new JTree(tree_model);
-		tree.setBounds(106, 141, 294, 220);
-		contentPane.add(tree);
-	}
-
+        String cmdStr = "git ";
+        Runtime run = Runtime.getRuntime();
+        try {
+            Process process = run.exec(cmdStr);
+            InputStream in = process.getInputStream();
+            InputStreamReader reader = new InputStreamReader(in);
+            BufferedReader br = new BufferedReader(reader);
+            StringBuffer sb = new StringBuffer();
+            String message;
+            while((message = br.readLine()) != null) {
+                sb.append(message);
+            }
+            System.out.println(sb);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 }
